0c23c687aee057e787173146d4ca52a910dbeeee
VBozev <shumurnik@gmail.com>
2023-05-18 02:15:14 +0300

    update3
    add basemap gallery; add search widget; additional functionality

--- map.js
+++ map.js
@@ -5,8 +5,11 @@ require([
     "esri/widgets/ScaleBar",
     "esri/widgets/Legend",
     "esri/widgets/Home",
-    "esri/widgets/LayerList"
-], (esriConfig, WebMap, MapView, ScaleBar, Legend, Home, LayerList) => {
+    "esri/widgets/LayerList",
+    "esri/widgets/BasemapToggle",
+    "esri/widgets/BasemapGallery",
+    "esri/widgets/Search"
+], (esriConfig, WebMap, MapView, ScaleBar, Legend, Home, LayerList, BasemapToggle, BasemapGallery, Search) => {
     esriConfig.apikey = "AAPKb65e0f9459e54524983d875ea23edb66IPXwyWdAtPXJM6BLx1WnCOHBtU-yslIHaLa2HvstGhbY8KK1_pbrIDvvKuWThivG";
 
     const webmap = new WebMap({
@@ -40,7 +43,26 @@ require([
 
     view.ui.add(scalebar, "bottom-left");
 
+    view.ui.add("basemap-btn", "top-right");
     view.ui.add("layerList-Btn", "top-right");
+
+    const basemapToggle = new BasemapToggle({
+        view: view,
+        nextBasemap: "arcgis-imagery"
+    })
+
+    const basemapGallery = new BasemapGallery({
+        view: view,
+        source: {
+            query: {
+
+            }
+        }
+    })
+
+    view.ui.add(basemapGallery, "top-right");
+
+    view.ui.add(basemapToggle, "bottom-right");
 
     const layerList = new LayerList({
         view: view
@@ -51,14 +73,36 @@ require([
     document
         .getElementById("layerList-Btn")
         .addEventListener("click", function () {
-            toggleButton()
+            toggleButton("layerList")
         })
 
-    function toggleButton() {
-        const layerListEl = document.getElementsByClassName("esri-layer-list") [0];
-        let currentProp = layerListEl.style.getPropertyValue("display");
+        document
+        .getElementById("basemap-btn")
+        .addEventListener("click", function() {
+            toggleButton("gallery");
+        })
 
-        layerListEl.style.setProperty("display", currentProp == "block" ? "none" : "block");
+        const searchWid = new Search({
+            view: view
+        })
+
+        view.ui.add(searchWid, "bottom-left");
+
+    function toggleButton(item) {
+        const layerListEl = document.getElementsByClassName("esri-layer-list") [0];
+        const galleryEl = document.getElementsByClassName("esri-basemap-gallery") [0];
+        let currentProp;
+
+        if(item == "layerList") {
+           currentProp = layerListEl.style.getPropertyValue("display");
+           layerListEl.style.setProperty("display", currentProp == "block" ? "none" : "block");
+           galleryEl.style.setProperty("display", "none");
+        } else if(item == "gallery") {
+            currentProp = galleryEl.style.getPropertyValue("display");
+            galleryEl.style.setProperty("display", currentProp == "block" ? "none" : "block");
+            layerListEl.style.setProperty("display", "none");
+
+        }
 
     }
 })
